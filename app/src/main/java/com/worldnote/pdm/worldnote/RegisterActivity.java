9379a0cbdfe89955819dfd8bf88609c907c47616
NewtonJ <newtonjoaquim12@gmail.com>
2018-04-26 00:03:24 -0300

    Linkado botao do Register

--- app/src/main/java/com/worldnote/pdm/worldnote/RegisterActivity.java
+++ app/src/main/java/com/worldnote/pdm/worldnote/RegisterActivity.java
@@ -2,12 +2,25 @@ package com.worldnote.pdm.worldnote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
